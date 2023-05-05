bf04bc5559b72bb5d2fac4cd60b5a592aa3d6f04
lucopdev <lucopdev@gmail.com>
2023-05-05 16:18:21 -0300

    feat: elementos da tela de login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <input type="email" data-testid="email-input" />
+      <input type="" data-testid="password-input" />
+      <button
+        data-testid="login-submit-btn"
+
+      >
+        Enter
+
+      </button>
+    </div>
+  );
+}
+
+export default Login;
